cd7b1deee07b625ee8a2400f495cccc985e66b4e
mac <mac@macs-MBP.fios-router.home>
2020-12-23 14:18:49 -0500

    Updates

---
+++ precache-manifest.9d218d4058fad7e950ac090b99d479b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba559a3acc74d9e1958303b89127617",
+    "url": "/cruise-prototype-two/index.html"
+  },
+  {
+    "revision": "5c9ff0699ba7b0804a8d",
+    "url": "/cruise-prototype-two/static/css/2.daed29bd.chunk.css"
+  },
+  {
+    "revision": "75a1565d4f7e77f64858",
+    "url": "/cruise-prototype-two/static/css/main.d410478e.chunk.css"
+  },
+  {
+    "revision": "5c9ff0699ba7b0804a8d",
+    "url": "/cruise-prototype-two/static/js/2.76493a97.chunk.js"
+  },
+  {
+    "revision": "c750f09f4e4762f4aea9",
+    "url": "/cruise-prototype-two/static/js/3.3dc62314.chunk.js"
+  },
+  {
+    "revision": "75a1565d4f7e77f64858",
+    "url": "/cruise-prototype-two/static/js/main.57db83c4.chunk.js"
+  },
+  {
+    "revision": "dd75cb706c8ac1a896a1",
+    "url": "/cruise-prototype-two/static/js/runtime-main.1a98b523.js"
+  }
+]);
